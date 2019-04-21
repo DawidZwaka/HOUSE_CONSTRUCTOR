a3a4b9348f023ec93c4af79c1d65d531af688beb
Dawid <zwakadawid@gmail.com>
2019-04-21 18:46:56 +0200

    adding some basic functionality

--- src/containers/HouseConstructor/HouseConstructor.js
+++ src/containers/HouseConstructor/HouseConstructor.js
@@ -4,6 +4,11 @@ import * as THREE from 'three.js';
 
 class HouseConstructor extends React.Component
 {
+    state = {
+        canRotate: false,
+        left: null,
+        cameraDistance: 4
+    } 
     componentDidMount()
     {
         const houseConstructor = this.refs['houseConstructor'];
@@ -16,7 +21,7 @@ class HouseConstructor extends React.Component
         this.renderer.setSize(width,height);
         houseConstructor.appendChild(this.renderer.domElement);
         
-        this.camera.position.z = 3;
+        this.camera.position.z = 4;
         this.camera.position.y = 1;
         this.camera.rotation.x = -Math.PI/10;
 
@@ -47,12 +52,58 @@ class HouseConstructor extends React.Component
         requestAnimationFrame(this.animate);
         this.renderer.render(this.scene, this.camera);
 
-        this.house.rotation.y += 0.01;
+        //this.house.rotation.y += 0.01;
     }
 
+    Rotate3DobjectHandler = (ev) => {
+        const left = ev.clientX - ev.target.offsetLeft;
+
+        if(this.state.canRotate)
+        {
+            const difference = this.state.left - left;
+
+            if(difference<0) this.house.rotation.y -= difference*0.01;
+            if(difference>0) this.house.rotation.y -= difference*0.01;
+
+            this.setState({left: left});
+        }
+    }
+
+    startRotatingHandler = (ev) => {
+        const left = ev.clientX - ev.target.offsetLeft;
+
+        this.setState({
+            canRotate: true,
+            left: left
+        });
+        
+    }
+
+    changeDistanceToTheObjectHandler = (ev) => {
+
+        
+        const distanceDifference = this.state.cameraDistance-ev.deltaY;
+        const HouseGeometryProps = this.house.geometry.parameters;
+        let cameraDistance = this.camera.position.z;
+        let minDistance = 0;
+        let maxDistance = 0;
+ 
+        if(HouseGeometryProps.width >= HouseGeometryProps.depth){
+            minDistance=HouseGeometryProps.width*1.7;
+            maxDistance=HouseGeometryProps.width*5;
+        }
+        else {
+            minDistance = HouseGeometryProps.depth*1.7;
+            maxDistance = HouseGeometryProps.depth*5;
+        }
+
+        if(distanceDifference>0 && cameraDistance>minDistance) this.camera.position.z += 0.005*ev.deltaY;
+        if(distanceDifference<0 && cameraDistance<maxDistance) this.camera.position.z += 0.005*ev.deltaY;
+    }
     render()
     {
-        return(<div style={{
+        return(
+        <div style={{
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center'
@@ -61,7 +112,11 @@ class HouseConstructor extends React.Component
             <div style={{
                 width: '90vw',
                 height: '40vh'
-            }} ref='houseConstructor'></div>
+            }} ref='houseConstructor'
+            onMouseDown={this.startRotatingHandler}
+            onMouseUp={() => this.setState({canRotate: false})}
+            onMouseMove={this.Rotate3DobjectHandler}
+            onWheel={this.changeDistanceToTheObjectHandler}></div>
         </div>);
     }
 }
